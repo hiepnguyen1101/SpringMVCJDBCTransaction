72ee54d823ac2a36729cae78043a4be251f1baf4
hiepuit <hiepnh95@gmail.com>
2017-05-08 22:55:00 +0700

    Init project

---
+++ src/main/java/org/o7planning/springmvcjdbc/mapper/DepartmentMapper.java
@@ -0,0 +1,24 @@
+package org.o7planning.springmvcjdbc.mapper;
+ 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+ 
+import org.o7planning.springmvcjdbc.model.Department;
+import org.springframework.jdbc.core.RowMapper;
+ 
+public class DepartmentMapper implements RowMapper<Department> {
+ 
+    public static final String BASE_SQL = //
+            "Select d.dept_id,d.dept_no,d.dept_name,d.location "//
+                    + " from Department d ";
+ 
+    public Department mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Integer deptId = rs.getInt("dept_id");
+        String deptNo = rs.getString("dept_no");
+        String deptName = rs.getString("dept_name");
+        String location = rs.getString("location");
+ 
+        return new Department(deptId, deptNo, deptName, location);
+    }
+ 
+}
